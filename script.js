727d3557dfd1dc3566afc2b95cd264f6495406fd
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-07-31 15:04:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // your code here
+button = document.querySelector('#button');
+
+button.addEventListener('click', ()=>{
+	heading = document.querySelector('#url');
+	name = document.querySelector('#name').value;
+	year = document.querySelector('#year').value;
+
+	let updatedHeading = heading.textContent + "?";
+	if(name!=="")updatedHeading += `name=${name}&`;
+	if(year!=="")updatedHeading += `year=${year}`;
+
+	updatedHeading = updatedHeading.endsWith('&')?updatedHeading.slice(0,-1):updatedHeading;
+	heading.textContent = updatedHeading;
+});
